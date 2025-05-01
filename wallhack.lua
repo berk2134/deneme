01e0f56a0206ab3e0903684fd06b7bdf6d38ce40
berk2134 <veledimtrak@gmail.com>
2025-05-01 20:34:00 +0300

    Create wallhack.lua

---
+++ wallhack.lua
@@ -0,0 +1,34 @@
+-- Wall hack için gerekli olan kod
+-- Bu kod, oyuncuların karakterlerini duvarların arkasında bile görünür hale getirecek
+
+local Players = game:GetService("Players")
+local RunService = game:GetService("RunService")
+
+-- Wall hack fonksiyonu
+local function enableWallHack(character)
+    -- Her parça üzerinde gezin
+    for _, part in pairs(character:GetDescendants()) do
+        if part:IsA("BasePart") then
+            -- Şeffaflığı 0 yaparak tüm parçaları görünür hale getir
+            part.Transparency = 0  -- Tam görünür
+            part.CanCollide = false  -- Çarpışmayı devre dışı bırak, duvarlardan geçebilmesi için
+        end
+    end
+end
+
+-- Oyuncu karakteri eklendiğinde, wall hack'i etkinleştir
+Players.PlayerAdded:Connect(function(player)
+    player.CharacterAdded:Connect(function(character)
+        -- Wall hack uygulama
+        enableWallHack(character)
+    end)
+end)
+
+-- Oyuncu karakterleri her karede kontrol edilerek wall hack'in sürekli olarak etkin kalması sağlanır
+RunService.RenderStepped:Connect(function()
+    for _, player in pairs(Players:GetPlayers()) do
+        if player.Character then
+            enableWallHack(player.Character)
+        end
+    end
+end)
